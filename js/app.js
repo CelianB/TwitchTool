944a2be0ce4109b83f679117bb33c4ff3c599e9c
hyrozen88 <bastini@hotmail.fr>
2017-08-06 19:40:22 +0200

    HTML Page v1
    without notif

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@
     var Param = {
         'clientId': 'qvr80zol9glws9vufdzujixvqg0oqs',
         'redirectUri': 'https://cbilfoloagcnhpgpjhojjfigjdpoflmg.chromiumapp.org/oauth2',
-        'Streamers':[]
+        'Streamers': []
     }
 
     AlertLive.prototype.getViewers = function (user) {
@@ -23,10 +23,10 @@
     }
 
     AlertLive.prototype.getUserFollowedChannels = async function (user) {
+        Param.Streamers = [];
         return new Promise((resolve, reject) => {
             var xhr = new XMLHttpRequest();
-            xhr.open('GET', 'https://api.twitch.tv/kraken/users/' + user + '/follows/channels?client_id=' + Param.clientId, true);
-            //xhr.open('GET', 'https://api.twitch.tv/kraken/users/' + user+ '/follows/channels?client_id=' + AlertLive.clientId+"?limit=100", true);
+            xhr.open('GET', 'https://api.twitch.tv/kraken/users/' + user + '/follows/channels?client_id=' + Param.clientId + '?&limit=100', true);
             xhr.onreadystatechange = function () {
                 if (xhr.readyState != 4 || xhr.status != 200) return;
                 var follows = JSON.parse(xhr.responseText);
@@ -39,27 +39,30 @@
                             if (xxhr[i].readyState != 4 || xxhr[i].status != 200) return;
                             var stream = JSON.parse(xxhr[i].responseText);
                             var Streamer = {
+                                display_name: follows.follows[i].channel.display_name,
                                 name: follows.follows[i].channel.name,
-                                img: follows.follows[i].channel.logo,
+                                img: follows.follows[i].channel.logo || 'https://static-cdn.jtvnw.net/jtv_user_pictures/xarth/404_user_70x70.png',
                                 notif: true,
-                                isLive:false,
-                                viewers:0
+                                isLive: false,
+                                viewers: 0
                             }
                             if (stream.stream != null) {
                                 Streamer.isLive = true;
-                                Streamer.description=stream.stream.channel.status;
-                                Streamer.game=stream.stream.game;
-                                Streamer.viewers=stream.stream.viewers;
+                                Streamer.description = stream.stream.channel.status;
+                                Streamer.game = stream.stream.game;
+                                Streamer.viewers = stream.stream.viewers;
                             }
                             Param.Streamers.push(Streamer);
+                            if (Param.Streamers.length == follows.follows.length - 1) {
+                                Param.Streamers = Param.Streamers.sort(function (a, b) {
+                                    return b.isLive - a.isLive || b.viewers - a.viewers || b.name - a.name;
+                                })
+                                resolve(Param.Streamers);
+                            }
                         }
                         xxhr[i].send();
                     })(i);
                 }
-                Param.Streamers=Param.Streamers.sort(function(a,b){
-                    return b.viewers - a.viewers;
-                })
-                resolve(Param.Streamers);
             }
             xhr.send();
         })
--- js/background.js
+++ js/background.js
@@ -1,11 +1,11 @@
 const username = document.querySelector('#username');
 const search = document.querySelector('#search');
+const ListStreams = document.querySelector('.streamers');
 
 chrome.storage.local.get(['username', 'allowNotification', 'notification', 'live'], function (res) {
     if (res.allowNotification || !res.hasOwnProperty('allowNotification'))
         res.allowNotification = true;
 });
-
 
 username.onkeydown = function (e) {
     if (e.keyCode == 13) {
@@ -29,7 +29,51 @@ function SaveUserName(Username) {
     }
 }
 
-async function Update(Username){
-   var Follows= await  AlertLive.getUserFollowedChannels(Username);
-   console.log(Follows);
+async function Update(Username) {
+    var Streams = await AlertLive.getUserFollowedChannels(Username).then(function (val) {
+        val.forEach(function (Streamer) {
+            BuildList(Streamer);
+        }, this);
+    })
+}
+
+function BuildList(Streamer) {
+    var li = document.createElement("li");
+    li.className = 'ripple';
+    var img = document.createElement('img');
+    img.src = Streamer.img;
+    img.style.cursor='pointer';
+    var divText = document.createElement('div');
+    divText.style.width = '95%';
+    divText.style.cursor='pointer';
+    var spanName = document.createElement('span');
+    spanName.className = 'name';
+    spanName.textContent = Streamer.display_name;
+    var spanDesc = document.createElement('span');
+    spanDesc.className = 'descr';
+    spanDesc.textContent = "OFFLINE";
+    var divCheck = document.createElement('div');
+    divCheck.className = 'notifdiv';
+    var Check = document.createElement('input');
+    Check.type = 'checkbox';
+    Check.checked=true;//notif
+    if (Streamer.isLive) {
+        spanName.textContent += ' [LIVE] sur ' + Streamer.game + ' (' + Streamer.viewers + ' viewers)';
+        spanDesc.textContent = Streamer.description;
+        spanDesc.title=Streamer.description;
+        li.className+=" live";
+        divText.onclick =function(){OpenStream(Streamer.name)} ;
+        img.onclick =function(){OpenStream(Streamer.name)} ;
+    }
+    divText.appendChild(spanName);
+    divText.appendChild(document.createElement("br"));
+    divText.appendChild(spanDesc);
+    divCheck.appendChild(Check);
+    li.appendChild(img);
+    li.appendChild(divText);
+    li.appendChild(divCheck);
+    ListStreams.appendChild(li);
+}
+function OpenStream(userName){
+    chrome.tabs.create({ url: 'https://www.twitch.tv/' + userName }, function (tab) { });
 }
