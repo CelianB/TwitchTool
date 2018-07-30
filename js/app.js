6a2c6faf3e688d78f983e95e6bdcfb682483095c
CelianB <celian-bastien@outlook.fr>
2018-07-30 18:07:28 +0200

    update

--- js/app.js
+++ js/app.js
@@ -1,99 +1,135 @@
-(function () {
-    var AlertLive = function () { };
-    var Param = {
-        'clientId': 'qvr80zol9glws9vufdzujixvqg0oqs',
-        'redirectUri': 'https://cbilfoloagcnhpgpjhojjfigjdpoflmg.chromiumapp.org/oauth2',
-        'notified': []
-    }
-    AlertLive.prototype.getUserFollowedChannels = user => {
-        streamers = [];
-        return new Promise(resolve => {
-            var xhr = new XMLHttpRequest();
-            xhr.open('GET', 'https://api.twitch.tv/kraken/users/' + user + '/follows/channels?&limit=100&client_id=' + Param.clientId, true);
-            xhr.onreadystatechange = () => {
-                if (xhr.readyState != 4 || xhr.status != 200) return;
-                var follows = JSON.parse(xhr.responseText);
-                var xxhr = [];
-                for (var i = 0; i < follows.follows.length; i++) {
-                    ((i) => {
-                        xxhr[i] = new XMLHttpRequest();
-                        xxhr[i].open('GET', 'https://api.twitch.tv/kraken/streams/' + follows.follows[i].channel.name + '?client_id=' + Param.clientId, true);
-                        xxhr[i].onreadystatechange = () => {
-                            if (xxhr[i].readyState != 4 || xxhr[i].status != 200) return;
-                            var stream = JSON.parse(xxhr[i].responseText);
-                            var Streamer = {
-                                display_name: follows.follows[i].channel.display_name,
-                                name: follows.follows[i].channel.name,
-                                img: follows.follows[i].channel.logo || 'https://static-cdn.jtvnw.net/jtv_user_pictures/xarth/404_user_70x70.png',
-                                isLive: false,
-                                viewers: 0,
-                                notified: false
-                            }
-                            if (stream.stream && stream.stream.stream_type == "live") {
-                                Streamer.isLive = true;
-                                Streamer.description = stream.stream.channel.status;
-                                Streamer.game = stream.stream.game;
-                                Streamer.viewers = stream.stream.viewers;
-                                if (Param.notified.indexOf(Streamer.name) !== -1) Streamer.notified = true;
-                            }
-                            else {
-                                var y = Param.notified.indexOf(Streamer.name);
-                                if (y !== -1) Param.notified.splice(y, 1);
-                            }
-                            streamers.push(Streamer);
-                            if (streamers.length == follows.follows.length - 1) {
-                                streamers = streamers.sort((a, b) => {
-                                    return b.isLive - a.isLive || b.viewers - a.viewers || b.name - a.name;
-                                })
-                                const IsOffLine = arg => arg.isLive == false;
-                                chrome.browserAction.setBadgeText({ "text": (streamers.length - streamers.filter(IsOffLine).length).toString() });
-                                resolve(streamers);
-                            }
-                        }
-                        xxhr[i].send();
-                    })(i);
-                }
-            }
-            xhr.send();
-        })
-    }
-
-    AlertLive.prototype.updateLives = (Onlines) => {
-        chrome.storage.local.get('streamer', (r) => {
-            var NoNotifStreamer = r.streamer;
-            Onlines.forEach(e => {
-                if ((NoNotifStreamer == undefined || NoNotifStreamer.indexOf(e.name) == -1) && (e.notified == false)) {
-                    Param.notified.push(e.name);
-                    var opt = {
-                        type: "basic",
-                        title: e.display_name,
-                        message: e.display_name + " just started a live stream",
-                        //iconUrl: e.img,
-                        iconUrl: '../img/icon64.png',
-                        isClickable: true
-                    };
-                    chrome.notifications.create(e.name, opt);
-                }
-            });
-        });
-    }
-
-    chrome.notifications.onClicked.addListener(id=>{
-        chrome.tabs.create({url:"https://www.twitch.tv/okazumaa"});
-    });
-
-    AlertLive.prototype.Updator = () => {
-        var IsOnline = stream => stream.isLive == true;
-        chrome.storage.local.get('TwitchUserName', r => {
-            AlertLive.prototype.getUserFollowedChannels(r.TwitchUserName).then(s => {
-                chrome.storage.local.get('TwitchToolNotif', r => {
-                    if(r.TwitchToolNotif)AlertLive.prototype.updateLives(s.filter(IsOnline));
+(function() {
+  var AlertLive = function() {};
+  var Param = {
+    clientId: "qvr80zol9glws9vufdzujixvqg0oqs",
+    redirectUri:
+      "https://cbilfoloagcnhpgpjhojjfigjdpoflmg.chromiumapp.org/oauth2",
+    notified: []
+  };
+  AlertLive.prototype.getUserFollowedChannels = user => {
+    streamers = [];
+    return new Promise(resolve => {
+      var xhr = new XMLHttpRequest();
+      xhr.open(
+        "GET",
+        "https://api.twitch.tv/kraken/users/" +
+          user +
+          "/follows/channels?&limit=100&client_id=" +
+          Param.clientId,
+        true
+      );
+      xhr.onreadystatechange = () => {
+        if (xhr.readyState != 4 || xhr.status != 200) return;
+        var follows = JSON.parse(xhr.responseText);
+        var xxhr = [];
+        for (var i = 0; i < follows.follows.length; i++) {
+          (i => {
+            xxhr[i] = new XMLHttpRequest();
+            xxhr[i].open(
+              "GET",
+              "https://api.twitch.tv/kraken/streams/" +
+                follows.follows[i].channel.name +
+                "?client_id=" +
+                Param.clientId,
+              true
+            );
+            xxhr[i].onreadystatechange = () => {
+              if (xxhr[i].readyState != 4 || xxhr[i].status != 200) return;
+              var stream = JSON.parse(xxhr[i].responseText);
+              var Streamer = {
+                display_name: follows.follows[i].channel.display_name,
+                name: follows.follows[i].channel.name,
+                img:
+                  follows.follows[i].channel.logo ||
+                  "https://static-cdn.jtvnw.net/jtv_user_pictures/xarth/404_user_70x70.png",
+                isLive: false,
+                viewers: 0,
+                notified: false
+              };
+              if (stream.stream && stream.stream.stream_type == "live") {
+                Streamer.stratedAt = stream.stream.created_at;
+                Streamer.isLive = true;
+                Streamer.description = stream.stream.channel.status;
+                Streamer.game = stream.stream.game;
+                Streamer.viewers = stream.stream.viewers;
+                if (Param.notified.indexOf(Streamer.name) !== -1)
+                  Streamer.notified = true;
+              } else {
+                var y = Param.notified.indexOf(Streamer.name);
+                if (y !== -1) Param.notified.splice(y, 1);
+              }
+              streamers.push(Streamer);
+              if (streamers.length == follows.follows.length - 1) {
+                streamers = streamers.sort((a, b) => {
+                  return (
+                    b.isLive - a.isLive ||
+                    b.viewers - a.viewers ||
+                    b.name - a.name
+                  );
                 });
-            });
+                const IsOffLine = arg => arg.isLive == false;
+                chrome.browserAction.setBadgeText({
+                  text: (
+                    streamers.length - streamers.filter(IsOffLine).length
+                  ).toString()
+                });
+                resolve(streamers);
+              }
+            };
+            xxhr[i].send();
+          })(i);
+        }
+      };
+      xhr.send();
+    });
+  };
+
+  AlertLive.prototype.updateLives = Onlines => {
+    chrome.storage.local.get("streamer", r => {
+      var NoNotifStreamer = r.streamer;
+      Onlines.forEach(e => {
+        if (
+          (NoNotifStreamer !== undefined ||
+            NoNotifStreamer.indexOf(e.name) == -1) &&
+          e.notified == false
+        ) {
+          let startDate = Date.parse(e.stratedAt);
+          let date = new Date();
+          let timeToAlert = date.setMinutes(date.getMinutes() - 10);
+          console.log(timeToAlert);
+          if (startDate > timeToAlert) {
+            var opt = {
+              type: "basic",
+              title: e.display_name,
+              message: e.display_name + " just started a live stream",
+              //iconUrl: e.img,
+              iconUrl: "../img/icon64.png",
+              isClickable: true
+            };
+            chrome.notifications.create(e.name, opt);
+          }
+        }
+      });
+    });
+  };
+  chrome.notifications.onClicked.addListener(id => {
+    chrome.tabs.create({ url: "https://www.twitch.tv/" + id });
+  });
+
+  AlertLive.prototype.Updator = () => {
+    var IsOnline = stream => stream.isLive == true;
+    chrome.storage.local.get("TwitchUserName", r => {
+      AlertLive.prototype.getUserFollowedChannels(r.TwitchUserName).then(s => {
+        chrome.storage.local.get("TwitchToolNotif", r => {
+          if (r.TwitchToolNotif === undefined || r.TwitchToolNotif) {
+            AlertLive.prototype.updateLives(s.filter(IsOnline));
+          }
         });
-    }
-    setInterval(() => {
-        AlertLive.prototype.Updator();
-    }, 60000);
-    window.AlertLive = new AlertLive();
+      });
+    });
+  };
+  setInterval(() => {
+    AlertLive.prototype.Updator();
+  }, 10000);
+  window.AlertLive = new AlertLive();
 })();
--- js/background.js
+++ js/background.js
@@ -1,127 +1,138 @@
-const username = document.querySelector('#username');
-const search = document.querySelector('#search');
-const allowNotif=document.querySelector('#allowNotif');
-const ListStreams = document.querySelector('.streamers');
+const username = document.querySelector("#username");
+const search = document.querySelector("#search");
+const allowNotif = document.querySelector("#allowNotif");
+const ListStreams = document.querySelector(".streamers");
 var StreamList;
 
-chrome.storage.local.get(['username', 'allowNotification', 'notification', 'live'], res => {
-    if (res.allowNotification || !res.hasOwnProperty('allowNotification'))
-        res.allowNotification = true;
-});
+chrome.storage.local.get(
+  ["username", "allowNotification", "notification", "live"],
+  res => {
+    if (res.allowNotification || !res.hasOwnProperty("allowNotification"))
+      res.allowNotification = true;
+  }
+);
 
 username.onkeydown = e => {
-    if (e.keyCode == 13) SaveUserName(username.value);
+  if (e.keyCode == 13) SaveUserName(username.value);
 };
 
 allowNotif.onchange = that => {
-    let notifON=that.target.checked
-    chrome.storage.local.set({ 'TwitchToolNotif':notifON});
-    for (var elem of ListStreams.children){
-        if (notifON)elem.getElementsByClassName("notifdiv")[0].firstChild.src = '../img/alarmON.png';
-        else elem.getElementsByClassName("notifdiv")[0].firstChild.src = '../img/alarmOFF.png';
-    }
+  let notifON = that.target.checked;
+  chrome.storage.local.set({ TwitchToolNotif: notifON });
+  for (var elem of ListStreams.children) {
+    if (notifON)
+      elem.getElementsByClassName("notifdiv")[0].firstChild.src =
+        "../img/alarmON.png";
+    else
+      elem.getElementsByClassName("notifdiv")[0].firstChild.src =
+        "../img/alarmOFF.png";
+  }
 };
 
 search.onclick = () => SaveUserName(username.value);
 
-
 window.onload = () => {
-    chrome.storage.local.get('TwitchUserName', r => {
-        if (r.TwitchUserName===undefined)
-            r.TwitchUserName="Twitch";
-        username.value = r.TwitchUserName;
-        Update(r.TwitchUserName);
-    });
-    chrome.storage.local.get('TwitchToolNotif', r => {
-        if (r.TwitchToolNotif===undefined)r.TwitchToolNotif=true;
-        allowNotif.checked=r.TwitchToolNotif;
-    });
-}
-const SaveUserName = username => {
-    if (username) {
-        chrome.storage.local.set({ 'TwitchUserName': username });
-        Update(username);
+  chrome.storage.local.get("TwitchUserName", r => {
+    if (r.TwitchUserName === undefined) r.TwitchUserName = "Twitch";
+    username.value = r.TwitchUserName;
+    Update(r.TwitchUserName);
+  });
+  chrome.storage.local.get("TwitchToolNotif", r => {
+    if (r.TwitchToolNotif === undefined) {
+      r.TwitchToolNotif = false;
+      //Disable alter by default
     }
-}
+    allowNotif.checked = r.TwitchToolNotif;
+  });
+};
+const SaveUserName = username => {
+  if (username) {
+    chrome.storage.local.set({ TwitchUserName: username });
+    Update(username);
+  }
+};
 async function Update(Username) {
-    ListStreams.innerHTML='';
-    var Streams = await AlertLive.getUserFollowedChannels(Username).then(val => {
-        StreamList = val;
-        val.forEach(Streamer => BuildList(Streamer), this);
-        chrome.storage.local.get('streamer', (r) => {
-            val.forEach((Streamer, i) => {
-                if (r.streamer!=undefined && r.streamer.indexOf(Streamer.name) !== -1)
-                    ListStreams.children[i].getElementsByClassName("notifdiv")[0].firstChild.src = '../img/alarmOFF.png';
-            }, this);
-        });
+  ListStreams.innerHTML = "";
+  var Streams = await AlertLive.getUserFollowedChannels(Username).then(val => {
+    StreamList = val;
+    val.forEach(Streamer => BuildList(Streamer), this);
+    chrome.storage.local.get("streamer", r => {
+      val.forEach((Streamer, i) => {
+        if (r.streamer != undefined && r.streamer.indexOf(Streamer.name) !== -1)
+          ListStreams.children[i].getElementsByClassName(
+            "notifdiv"
+          )[0].firstChild.src =
+            "../img/alarmOFF.png";
+      }, this);
     });
+  });
 }
 
 const BuildList = Streamer => {
-    var li = document.createElement("li");
-    li.className = 'stream';
-    var img = document.createElement('img');
-    img.src = Streamer.img;
-    img.style.cursor = 'pointer';
-    var divText = document.createElement('div');
-    divText.style.width = '95%';
-    divText.style.cursor = 'pointer';
-    var spanName = document.createElement('span');
-    spanName.className = 'name';
-    spanName.textContent = Streamer.display_name;
-    var spanDesc = document.createElement('span');
-    spanDesc.className = 'descr';
-    spanDesc.textContent = "OFFLINE";
-    var divNotif = document.createElement('div');
-    divNotif.className = 'notifdiv';
-    var Notif = document.createElement('img');
-    Notif.src = '../img/alarmOFF.png';
-    Notif.style.cursor = 'pointer';
-    Notif.style.marginTop = '-8px';
-    Notif.onclick = (e) => {
-        if (e.path[0].src.indexOf("OFF.png") != -1) {
-            e.path[0].src = '../img/alarmON.png';
-            deleteNoNotif(Streamer.name)
-        }
-        else {
-            e.path[0].src= '../img/alarmOFF.png';
-            addNonotif(Streamer.name)
-        }
+  var li = document.createElement("li");
+  li.className = "stream";
+  var img = document.createElement("img");
+  img.src = Streamer.img;
+  img.style.cursor = "pointer";
+  var divText = document.createElement("div");
+  divText.style.width = "95%";
+  divText.style.cursor = "pointer";
+  var spanName = document.createElement("span");
+  spanName.className = "name";
+  spanName.textContent = Streamer.display_name;
+  var spanDesc = document.createElement("span");
+  spanDesc.className = "descr";
+  spanDesc.textContent = "OFFLINE";
+  var divNotif = document.createElement("div");
+  divNotif.className = "notifdiv";
+  var Notif = document.createElement("img");
+  Notif.src = "../img/alarmOFF.png";
+  Notif.style.cursor = "pointer";
+  Notif.style.marginTop = "-8px";
+  Notif.onclick = e => {
+    if (e.path[0].src.indexOf("OFF.png") != -1) {
+      e.path[0].src = "../img/alarmON.png";
+      deleteNoNotif(Streamer.name);
+    } else {
+      e.path[0].src = "../img/alarmOFF.png";
+      addNonotif(Streamer.name);
     }
-    if (Streamer.isLive) {
-        spanName.textContent += ' [LIVE] sur ' + Streamer.game + ' (' + Streamer.viewers + ' viewers)';
-        spanDesc.textContent = Streamer.description;
-        spanDesc.title = Streamer.description;
-        li.className += " live";
-    }
-    else li.style.backgroundColor = '#e2e0e0';
-    divText.onclick = () => OpenStream(Streamer.name);
-    img.onclick = () => OpenStream(Streamer.name);
-    divText.appendChild(spanName);
-    divText.appendChild(document.createElement("br"));
-    divText.appendChild(spanDesc);
-    divNotif.appendChild(Notif);
-    li.appendChild(img);
-    li.appendChild(divText);
-    li.appendChild(divNotif);
-    ListStreams.appendChild(li);
-}
-const OpenStream = userName => chrome.tabs.create({ url: 'https://www.twitch.tv/' + userName });
+  };
+  if (Streamer.isLive) {
+    spanName.textContent +=
+      " [LIVE] on " + Streamer.game + " (" + Streamer.viewers + " viewers)";
+    spanDesc.textContent = Streamer.description;
+    spanDesc.title = Streamer.description;
+    li.className += " live";
+  } else li.style.backgroundColor = "#e2e0e0";
+  divText.onclick = () => OpenStream(Streamer.name);
+  img.onclick = () => OpenStream(Streamer.name);
+  divText.appendChild(spanName);
+  divText.appendChild(document.createElement("br"));
+  divText.appendChild(spanDesc);
+  divNotif.appendChild(Notif);
+  li.appendChild(img);
+  li.appendChild(divText);
+  li.appendChild(divNotif);
+  ListStreams.appendChild(li);
+};
+const OpenStream = userName =>
+  chrome.tabs.create({ url: "https://www.twitch.tv/" + userName });
 
 const deleteNoNotif = name => {
-    chrome.storage.local.get('streamer', (r) => {
-        var streamer = r.streamer;
-        streamer.splice(streamer.indexOf(name), 1);
-        chrome.storage.local.set({ streamer: streamer });
-    });
-}
+  chrome.storage.local.get("streamer", r => {
+    var streamer = r.streamer;
+    streamer.splice(streamer.indexOf(name), 1);
+    chrome.storage.local.set({ streamer: streamer });
+  });
+};
 const addNonotif = name => {
-    chrome.storage.local.get('streamer', (r) => {
-        var streamer = r.streamer;
-        if (streamer==undefined)streamer=[];
-        if (streamer.indexOf(name) == -1) {
-            streamer.push(name)
-            chrome.storage.local.set({ streamer: streamer });
-        }
-    });
-}
+  chrome.storage.local.get("streamer", r => {
+    var streamer = r.streamer;
+    if (streamer == undefined) streamer = [];
+    if (streamer.indexOf(name) == -1) {
+      streamer.push(name);
+      chrome.storage.local.set({ streamer: streamer });
+    }
+  });
+};
